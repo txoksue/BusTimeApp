a5be3d007e187721389d2884ea0aff0f2dd5194e
Jose Ramon Parreño Bonillo <jose.parreno.bonillo@accenture.com>
2019-08-27 17:44:34 +0200

    Spring annotations removed.

--- src/main/java/com/txoksue/bustime/services/EMTRestServiceImpl.java
+++ src/main/java/com/txoksue/bustime/services/EMTRestServiceImpl.java
@@ -4,22 +4,19 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.txoksue.bustime.exception.TimeBusException;
 import com.txoksue.bustime.model.ResponseBusTimeData;
 import com.txoksue.bustime.model.ResponseTokenData;
 
-@Service("emtRestService")
+
 public class EMTRestServiceImpl implements EMTRestService {
 
 	private static Logger logger = LoggerFactory.getLogger(EMTRestServiceImpl.class);
