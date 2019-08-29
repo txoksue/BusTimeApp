938e33534f5025cb6f3dce1a02190aa60b4d28a8
Jose Ramon Parreño Bonillo <jose.parreno.bonillo@accenture.com>
2019-08-29 17:06:20 +0200

    New service manages speech text.

---
+++ src/main/java/com/txoksue/bustime/services/SpeechService.java
@@ -0,0 +1,9 @@
+package com.txoksue.bustime.services;
+
+import com.txoksue.bustime.model.ArriveBusTime;
+
+public interface SpeechService {
+
+	
+	String getSpeechEstimateArrive(ArriveBusTime arrive);
+}
---
+++ src/main/java/com/txoksue/bustime/services/SpeechServiceImpl.java
@@ -0,0 +1,32 @@
+package com.txoksue.bustime.services;
+
+import com.txoksue.bustime.model.ArriveBusTime;
+
+public class SpeechServiceImpl implements SpeechService {
+
+	@Override
+	public String getSpeechEstimateArrive(ArriveBusTime arrive) {
+		
+		String speechText;
+		
+		int minutes = arrive.getEstimateArrive() / 60;
+		
+		if (arrive.getEstimateArrive() > 999999) {
+			
+			speechText = "¡Tranquilo!, te quedan más de 20 minutos.";
+		
+		}else if (minutes > 5) {
+
+			String coletilla = (minutes > 10)? "todavía tienes tiempo, no te duermas." : "date prisa, mueve tu culo.";
+			
+			speechText = "El autobús llegará en " + minutes + " minutos, " + coletilla;
+		
+		}else {
+			
+			speechText = "¡Lo has perdido!. La próxima vez ¡muévete más rápido!.";
+		}
+		
+		return speechText;
+	}
+
+}
