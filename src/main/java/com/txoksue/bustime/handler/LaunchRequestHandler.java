3e3bd759803b1e91c695bdf0070585fa25cd9c3e
Jose Ramon Parreño Bonillo <jose.parreno.bonillo@accenture.com>
2019-08-29 17:03:47 +0200

    Getting yaml properties and access toke EMT to put into session attributes.

--- src/main/java/com/txoksue/bustime/handler/LaunchRequestHandler.java
+++ src/main/java/com/txoksue/bustime/handler/LaunchRequestHandler.java
@@ -2,19 +2,33 @@ package com.txoksue.bustime.handler;
 
 import static com.amazon.ask.request.Predicates.requestType;
 
+import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.amazon.ask.attributes.AttributesManager;
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
 import com.amazon.ask.model.LaunchRequest;
 import com.amazon.ask.model.Response;
+import com.txoksue.bustime.exception.TimeBusException;
+import com.txoksue.bustime.services.EMTRestService;
+import com.txoksue.bustime.services.EMTRestServiceImpl;
+import com.txoksue.bustime.services.LoadYamlService;
+import com.txoksue.bustime.services.LoadYamlServiceImpl;
 
 public class LaunchRequestHandler implements RequestHandler {
-	
+
 	private static final Logger logger = LogManager.getLogger(LaunchRequestHandler.class);
+
+	private static final String SPEECH_ERROR = "Ahora mismo no te puedo ayudar. Alguien ha pisado un cable.";
+
+	private LoadYamlService loadYamlService = new LoadYamlServiceImpl();
+	
+	private EMTRestService emtRestService = new EMTRestServiceImpl();
 
 	@Override
 	public boolean canHandle(HandlerInput input) {
@@ -23,11 +37,41 @@ public class LaunchRequestHandler implements RequestHandler {
 
 	@Override
 	public Optional<Response> handle(HandlerInput input) {
-		
-		String repromptText = "";
-		String text = "Bienvenido a Tiempo bus. ¿En qué puedo ayudarte?";
-		return input.getResponseBuilder().withSpeech(text).withReprompt("Hola").withShouldEndSession(false).build();
 
+		logger.info("Managing launch request.");
+
+		AttributesManager attributesManager = input.getAttributesManager();
+		Map<String, Object> sessionAttributes = attributesManager.getSessionAttributes();
+		
+		Map<String, Object> yamlProperties = loadYamlService.load();
+		
+		logger.info(yamlProperties);
+		
+		sessionAttributes.put("properties", yamlProperties);
+
+		try {
+
+			String accessToken = emtRestService.getAccessToken();
+
+			if (Objects.nonNull(accessToken)) {
+
+				logger.info("Token: {}", accessToken);
+
+				sessionAttributes.put("accessToken", accessToken);
+
+				String repromptText = "";
+				String text = "Bienvenido a Tiempo bus. ¿En qué puedo ayudarte?";
+
+				return input.getResponseBuilder().withSpeech(text).withReprompt("Hola").withShouldEndSession(false)
+						.build();
+			}
+
+		} catch (TimeBusException e) {
+
+			logger.error("Error recuperando el token. {}", e.getMessage());
+		}
+
+		return input.getResponseBuilder().withSpeech(SPEECH_ERROR).withShouldEndSession(true).build();
 	}
 
 }
