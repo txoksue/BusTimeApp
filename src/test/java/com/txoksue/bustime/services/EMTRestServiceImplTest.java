3dd8b26337601f05b23964a9572324342107408c
Jose Ramon Parreño Bonillo <jose.parreno.bonillo@accenture.com>
2019-08-29 14:00:33 +0200

    Object changed

--- src/test/java/com/txoksue/bustime/services/EMTRestServiceImplTest.java
+++ src/test/java/com/txoksue/bustime/services/EMTRestServiceImplTest.java
@@ -1,19 +1,25 @@
 package com.txoksue.bustime.services;
 
+import static org.junit.Assert.assertNotNull;
+
 import java.io.IOException;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.txoksue.bustime.model.ResponseTokenData;
+import com.txoksue.bustime.model.TokenData;
 
 @RunWith(MockitoJUnitRunner.class)
 public class EMTRestServiceImplTest {
+	
+	private static final Logger logger = LogManager.getLogger(EMTRestServiceImplTest.class);
 
-	private static final String JSON = "{\"code\": \"01\", \"description\": \"Token 9044ac8d-6ffc-4ea9-95d4-4e60e013edb6 extend  into control-cache\", \"datetime\": \"2019-08-28T18:02:43.577016\", \"data\": [{\"internalUser\": \"dHhva3N1ZTgwQGdtYWlsLmNvbTpaRWo0RWc2MkNjUjlBdGc=\", \"updatedAt\": \"2019-07-15T19:18:41.2030000\", \"userName\": \"txoksue\", \"idUser\": \"2b7e1421-8218-4c1e-b069-f6dd1b90dafe\", \"tokenSecExpiration\": 968, \"email\": \"txoksue80@gmail.com\", \"accessToken\": \"9044ac8d-6ffc-4ea9-95d4-4e60e013edb6\", \"apiCounter\": {\"current\": 61, \"dailyUse\": 150000}}]}";
+	private static final String JSON_EXAMPLE = "{\"code\": \"01\", \"description\": \"Token 9044ac8d-6ffc-4ea9-95d4-4e60e013edb6 extend  into control-cache\", \"datetime\": \"2019-08-28T18:02:43.577016\", \"data\": [{\"internalUser\": \"dHhva3N1ZTgwQGdtYWlsLmNvbTpaRWo0RWc2MkNjUjlBdGc=\", \"updatedAt\": \"2019-07-15T19:18:41.2030000\", \"userName\": \"txoksue\", \"idUser\": \"2b7e1421-8218-4c1e-b069-f6dd1b90dafe\", \"tokenSecExpiration\": 968, \"email\": \"txoksue80@gmail.com\", \"accessToken\": \"9044ac8d-6ffc-4ea9-95d4-4e60e013edb6\", \"apiCounter\": {\"current\": 61, \"dailyUse\": 150000}}]}";
 
 	private ObjectMapper objectMapper;
 	
@@ -27,11 +33,14 @@ public class EMTRestServiceImplTest {
 	public void jsonConvert() {
 		
 		try {
-			ResponseTokenData response = objectMapper.readValue(JSON, ResponseTokenData.class);
+			
+			TokenData response = objectMapper.readValue(JSON_EXAMPLE, TokenData.class);
+			
+			assertNotNull(response);
 			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+			logger.error("Error parsing json to TokenData object.");
 		}
 	}
 
