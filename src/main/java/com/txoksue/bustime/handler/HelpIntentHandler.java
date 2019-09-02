f0e0ffcbb59d38af259f2ea813190912bda6293e
Jose Ramon Parreño Bonillo <jose.parreno.bonillo@accenture.com>
2019-09-02 17:50:31 +0200

    New HelpIntentHandler added.

---
+++ src/main/java/com/txoksue/bustime/handler/HelpIntentHandler.java
@@ -0,0 +1,31 @@
+package com.txoksue.bustime.handler;
+
+import static com.amazon.ask.request.Predicates.intentName;
+import java.util.Optional;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.dispatcher.request.handler.impl.IntentRequestHandler;
+import com.amazon.ask.model.IntentRequest;
+import com.amazon.ask.model.Response;
+
+public class HelpIntentHandler implements IntentRequestHandler {
+	
+	private static final Logger logger = LogManager.getLogger(HelpIntentHandler.class);
+
+	@Override
+	public boolean canHandle(HandlerInput input, IntentRequest intentRequest) {
+		return input.matches(intentName("AMAZON.HelpIntent"));
+	}
+
+	@Override
+	public Optional<Response> handle(HandlerInput input, IntentRequest intentRequest) {
+		
+		logger.info("Handling HelpIntent");
+		
+		return input.getResponseBuilder().withSpeech("Puedes preguntarme sobre el tiempo de llegada de los autobuses de la <say-as interpret-as=\"spell-out\">EMT</say-as>. Pero ten en cuenta, que sólo conozco el 175 y 178 de momento.").withShouldEndSession(false).build();
+	}
+
+}
