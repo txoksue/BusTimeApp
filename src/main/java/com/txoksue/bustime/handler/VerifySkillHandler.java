0ace6319fac29c154e4b3c8f2479f84bcc091441
Jose Ramon Parreño Bonillo <jose.parreno.bonillo@accenture.com>
2019-09-02 17:51:01 +0200

    New handlers added.

--- src/main/java/com/txoksue/bustime/handler/VerifySkillHandler.java
+++ src/main/java/com/txoksue/bustime/handler/VerifySkillHandler.java
@@ -8,16 +8,18 @@ public class VerifySkillHandler extends SkillStreamHandler {
 	
 	//Forcing to load the logging file configuration lambda-log4j2.xml
 	static {
-	    System.setProperty("log4j.configurationFile", "resources/lambda-log4j2.xml");
+	    System.setProperty("log4j.configurationFile", "lambda-log4j2.xml");
 	}
 
 	private static Skill getSkill() {
         return Skills.standard()
         			.withSkillId("amzn1.ask.skill.bd3b7784-8b70-484a-8415-61892c5a8ab9")
                 .addRequestHandlers(
+                	    new LaunchRequestHandler(), 
+                	    new HelpIntentHandler(),
                     new CancelandStopIntentHandler(),
-                    new BusTimeIntentHandler(),
-                    new LaunchRequestHandler())
+                    new SessionEndedRequestHandler(),
+                    new BusTimeIntentHandler())
                 .build();
     }
 
