2c99217c0dee13ad920164d7475062231a9b630d
Jose Ramon Parreño Bonillo <jose.parreno.bonillo@accenture.com>
2019-09-03 17:54:24 +0200

    New handlers and interceptors added.

--- src/main/java/com/txoksue/bustime/handler/VerifySkillHandler.java
+++ src/main/java/com/txoksue/bustime/handler/VerifySkillHandler.java
@@ -3,6 +3,8 @@ package com.txoksue.bustime.handler;
 import com.amazon.ask.Skill;
 import com.amazon.ask.SkillStreamHandler;
 import com.amazon.ask.Skills;
+import com.txoksue.bustime.interceptor.LogRequestInterceptor;
+import com.txoksue.bustime.interceptor.LogResponseInterceptor;
 
 public class VerifySkillHandler extends SkillStreamHandler {
 	
@@ -14,6 +16,9 @@ public class VerifySkillHandler extends SkillStreamHandler {
 	private static Skill getSkill() {
         return Skills.standard()
         			.withSkillId("amzn1.ask.skill.bd3b7784-8b70-484a-8415-61892c5a8ab9")
+        			.addRequestInterceptor(new LogRequestInterceptor())
+        			.addResponseInterceptor(new LogResponseInterceptor())
+        			.addExceptionHandler(new ExceptionsHandler())
                 .addRequestHandlers(
                 	    new LaunchRequestHandler(), 
                 	    new HelpIntentHandler(),
